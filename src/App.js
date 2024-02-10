730113c15faaec307642f07bc360a3d4dd847883
AnnaHuk05 <annavonhuk@gmail.com>
2024-02-11 00:23:10 +0200

    [try to fix mainPage path]

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LotInfoPage from "./pages/LotInfo/LotInfoPage.tsx";
-import MainAuctionPage from "./pages/mainAuctionPage/mainAuctionPage.tsx";
+import MainAuctionPage from "./pages/mainAuction/mainAuctionPage.tsx";
 //import Header from "./components/header/header.tsx";
 import './App.css';
 
