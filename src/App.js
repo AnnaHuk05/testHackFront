3fd1e415b8e1070bf717d31c501df6bbb433bba7
AnnaHuk05 <annavonhuk@gmail.com>
2024-02-10 14:29:32 +0200

    Added router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 
-import Header from './components/header/header.tsx';
-import PhotoViewer from './components/photoViewer/photoViewer.tsx';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LotInfoPage from "./pages/LotInfo/lotInfoPage.tsx";
 import './App.css';
 
 function App() {
-  return (
-   <><Header />
-   <PhotoViewer/></>
+  return(
+    <Router>
+     
+        <Routes>
+          <Route path="/" element={<LotInfoPage />} />
+         
+        </Routes>
+    </Router>
   );
+  
+ 
 }
 
 export default App;
---
+++ src/pages/LotInfo/LotInfoPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './../../components/header/header.tsx';
+import PhotoViewer from './../../components/photoViewer/photoViewer.tsx';
+
+function LotInfoPage()
+{
+    return (
+        <><Header />
+        <PhotoViewer/></>
+       );
+}
+
+export default LotInfoPage;
