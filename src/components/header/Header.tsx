2af9ea9302424564fc36bb2fe4500834b3a71d67
AnnaHuk05 <annavonhuk@gmail.com>
2024-02-10 13:54:16 +0200

    Added search bar and photoViewer

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,24 +1,44 @@
 import React from "react";
+import Search from "../searchField/search.tsx";
+import "./header.css";
 
-function Header()
-{
-    return(
-        <header className="header">
-             <div className="container">
-                <div className="header_row">
-                    <div className="logo">
-                    <img
-                        src="../../../public/logo-icon.svg"
-                        width={"24px"}
-                        height={"24px"}
-                        alt="Edit icon"
-                         />
-                       
-                        </div> 
-                    <nav className="header-nav"></nav>
-                </div>
-             </div>
-        </header>
-   
-);
-    }
+function Header() {
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="header_row">
+          <div className="logo">
+            <img
+              src="logo-icon.svg"
+              width={"140px"}
+              height={"100px"}
+              alt="Edit icon"
+            />
+          </div>
+          <div className="header-nav">
+           
+            <a href="#!">
+              <img
+                src="chat-icon.png"
+                alt="chat-item"
+                width={"35px"}
+                height={"35px"}
+              />
+            </a>
+            <Search />
+            <a href="#!">
+              <img
+                src="profile-icon.svg"
+                alt="profile-item"
+                width={"35px"}
+                height={"35px"}
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/photoViewer/photoViewer.tsx
@@ -0,0 +1,66 @@
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import React from 'react';
+import "./photoViewer.css";
+
+
+  const images = [
+    "https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
+    "https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80",
+    "https://images.unsplash.com/photo-1536987333706-fc9adfb10d91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
+  ];
+  
+  function photoViewer () {
+      return (
+        <div className="box">
+          <Carousel
+            showIndicators
+            renderArrowNext={(clickHandler, hasNext) => {
+              return (
+                hasNext && (
+                  <button className="nav_btn nav_btn_right" onClick={clickHandler}>
+                    <svg>
+                      <use xlinkHref={"sprite.svg" + "#right"}></use>
+                    </svg>
+                  </button>
+                )
+              );
+            }}
+            renderArrowPrev={(clickHandler, hasNext) => {
+              return (
+                hasNext && (
+                  <button onClick={clickHandler} className="nav_btn nav_btn_left">
+                    <svg>
+                      <use xlinkHref={"sprite.svg" + "#left"}></use>
+                    </svg>
+                  </button>
+                )
+              );
+            }}
+            renderIndicator={(clickHandler, isSelected, index) => {
+              return (
+                <li
+                  onClick={clickHandler}
+                  className={`ind ${isSelected ? "active" : ""}`}
+                  key={index}
+                  role="button"
+                />
+              );
+            }}
+          
+            transitionTime={310}
+        
+            swipeable={false}        
+          >
+            {images.map((URL, index) => (
+              <div className="slide">
+                <img alt="sample_file" src={URL} key={index} />
+              </div>
+            ))}
+          </Carousel>
+        </div>
+      );
+    }
+  
+
+  export default photoViewer;
